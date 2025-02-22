673fe10358620f40a1dc76a677437588cecafdfb
jqsmiin <jasmin.oruc23@gmail.com>
2025-02-22 09:31:14 +0100

    bg changed

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -20,7 +20,7 @@ export default function OnBoardingPage() {
   };
 
   return (
-    <div className='min-h-screen bg-gray-900 flex items-center justify-center p-4'>
+    <div className='min-h-screen bg-[#292C35] flex items-center justify-center p-4'>
       <div className='w-full max-w-md'>
         <div className='h-[10px]'>
           <ProgressBar progress={progress} />
