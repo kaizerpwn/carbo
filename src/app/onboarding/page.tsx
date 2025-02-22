76bae1e7d02de72c9cc6c90900d6e73429e27acc
jqsmiin <jasmin.oruc23@gmail.com>
2025-02-22 21:25:32 +0100

    Removed progress bar from onboarding page

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import WelcomeStep from "../../components/onboarding/welcome-step";
-import TransportStep from "../../components/onboarding/transport-step";
-import ProgressBar from "@/components/Progresbar";
-import ReusableBags from "../../components/onboarding/reusable-bags";
-import RecycleBehave from "../../components/onboarding/recycle-behave";
-import CompletionPage from "../../components/onboarding/finish-page";
-import { useAuth } from "@/context/AuthContext";
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import WelcomeStep from '../../components/onboarding/welcome-step';
+import TransportStep from '../../components/onboarding/transport-step';
+import ProgressBar from '@/components/Progresbar';
+import ReusableBags from '../../components/onboarding/reusable-bags';
+import RecycleBehave from '../../components/onboarding/recycle-behave';
+import CompletionPage from '../../components/onboarding/finish-page';
+import { useAuth } from '@/context/AuthContext';
 
 export default function OnBoardingPage() {
   const [currentStep, setCurrentStep] = useState(0);
@@ -24,15 +24,15 @@ export default function OnBoardingPage() {
     if (currentStep < totalSteps - 1) {
       setCurrentStep((prev) => prev + 1);
     } else {
-      router.push("/sign-up");
+      router.push('/sign-up');
     }
   };
 
   return (
-    <div className="min-h-screen bg-backgroundDark flex items-center justify-center p-4">
-      <div className="w-full max-w-md">
-        <div className="h-[10px]">
-          <ProgressBar progress={progress} />
+    <div className='min-h-screen bg-backgroundDark flex items-center justify-center p-4'>
+      <div className='w-full max-w-md'>
+        <div className='h-[10px]'>
+          {progress == 0 ? <></> : <ProgressBar progress={progress} />}
         </div>
 
         {currentStep === 0 ? (
