f9e958698697031d8eb745a11e16fe2bd02b32ca
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-02-22 11:48:58 +0100

    Add transport, energy, and recycle fields to User model; update registration logic

--- src/lib/Auth/Auth.ts
+++ src/lib/Auth/Auth.ts
@@ -5,11 +5,32 @@ export interface UserLoginValues {
   password: string;
 }
 
+export interface RegisterUserValues {
+  email: string;
+  password: string;
+  username: string;
+  fullName: string;
+  country: string;
+  town: string;
+  transport: string;
+  energy: string;
+  recycle: string;
+}
+
 export const AuthAPI = {
   loginUser: async (values: UserLoginValues) => {
-    const response = await http.post("/api/auth/login", values);
+    const response = await http.post("/auth/login", values);
     if (response.status !== 200) {
       throw new Error("Login failed");
+    }
+    return response.data;
+  },
+
+  registerUser: async (values: RegisterUserValues) => {
+    console.log("values", values);
+    const response = await http.post("/auth/register", values);
+    if (response.status !== 200) {
+      throw new Error("Registration failed");
     }
     return response.data;
   },
