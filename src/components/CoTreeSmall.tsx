50a00d4630925a83ad67aa5e93c88f22afb7f340
Izet Delibasic <izetdelibasic55@gmail.com>
2025-02-22 12:22:12 +0100

    Creating pop-up messages for new users

---
+++ src/components/CoTreeSmall.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const CoTreeSmall: React.FC = () => {
+  return (
+    <svg
+      width="583"
+      height="613"
+      viewBox="0 0 583 613"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M260 436.5L240.5 420L216 427L259 472.5L253 613H316.5L311.5 493L369.5 431.5L367.5 411L310.5 459L308.5 397L261.5 394.5L260 436.5Z"
+        fill="#E88687"
+      />
+      <circle cx="336.5" cy="322.5" r="99.5" fill="#6FD4A1" />
+      <ellipse cx="483.5" cy="303.5" rx="99.5" ry="103.5" fill="#6FD4A1" />
+      <ellipse cx="436.5" cy="158.5" rx="99.5" ry="103.5" fill="#6FD4A1" />
+      <circle cx="299.5" cy="103.5" r="103.5" fill="#6FD4A1" />
+      <circle cx="179.5" cy="158.5" r="103.5" fill="#6FD4A1" />
+      <circle cx="103.5" cy="284.5" r="103.5" fill="#6FD4A1" />
+      <circle cx="198.5" cy="326.5" r="103.5" fill="#6FD4A1" />
+      <circle cx="295.5" cy="215.5" r="103.5" fill="#6FD4A1" />
+    </svg>
+  );
+};
+
+export default CoTreeSmall;
